7677b2812f74d0ba7f2e60c1a61d8d3e3c6c185c
Juan Carlos <juanca0423@gmail.com>
2026-06-07 22:09:01 -0600

    fix(oil): guard vim.diagnostic.disable and close cheatsheet file properly

--- lua/plugins/boilnvim.lua
+++ lua/plugins/boilnvim.lua
@@ -16,5 +16,18 @@ return {
 			},
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Abrir Oil" })
+
+		-- Workaround: disable diagnostics inside Oil buffers to avoid
+		-- diagnostic reads on ephemeral explorer buffers which can
+		-- trigger "Index out of bounds" in some versions/plugins.
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "oil",
+			callback = function(args)
+				if vim.diagnostic and type(vim.diagnostic.disable) == "function" then
+					-- disable diagnostics for this buffer (no specific namespace)
+					vim.diagnostic.disable(nil, args.buf)
+				end
+			end,
+		})
 	end,
 }
